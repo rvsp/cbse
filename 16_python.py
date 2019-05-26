680031f44ca730416ebcfec7693ca7c6058a53b5
tactfulness <pvenkat.rv@gmail.com>
2019-05-26 22:34:25 +0530

    16_python.py

---
+++ 16_python.py
@@ -0,0 +1,3 @@
+# 16) Write a python script to store name and telephone numbers in a text file and
+# search for a specific name and change the telephone number of that name
+# with new number. If the name is not found display error Message.

0d7edd9972cb827937933318b478c4b524eb2f80
tactfulness <pvenkat.rv@gmail.com>
2019-05-26 21:47:43 +0530

    13_python.py

--- 13_python.py
+++ 13_python.py
@@ -1,6 +1,11 @@
+# 13) Write a python script to copy a text files source.txt into another file target.txt
+# except the lines starting with '@'
 
+with open('source.txt') as fsource:
+    with open("target.txt", "w") as fdest:
+        for line in fsource:
+            if not line.startswith('@'):
+                fdest.write(line)  
 
-with open("test.txt") as f:
-    with open("out.txt", "w") as f1:
-        for line in f:
-            f1.write(line)
+fsource.close()
+fdest.close()

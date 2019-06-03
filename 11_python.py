60a8fa3daf7f2106731f2eeebe6904469a634f0c
tactfulness <pvenkat.rv@gmail.com>
2019-06-03 23:23:13 +0530

    11_python.py

--- 11_python.py
+++ 11_python.py
@@ -1,8 +1,12 @@
 # 11) Write a python script to store n lines in a file and displays the longest line in the file
 
-# to open file in read mode
-f=open('sample.txt','r')
-# print max length of line from file
-print(max(f))
 
-f.close()
+large_line = ''
+large_line_len = 0
+f = open('sample.txt', 'r')
+for line in f:
+    if large_line_len < len(line):
+        large_line_len = len(line)
+        large_line = line
+
+print(large_line)
